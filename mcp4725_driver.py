660ddc63b6e5df50f8325331a7ef20b4c58db6b4
Василий Шишкин <shishkin.vv@phystech.edu>
2026-03-03 13:12:35 +0300

    mcp4725_driver.py added

---
+++ mcp4725_driver.py
@@ -0,0 +1,53 @@
+import RPi.GPIO as GPIO
+import smbus
+
+class MCP4725:
+    def __init__(self, dynamic_range, address=0x61, verbose = True):
+        self.bus = smbus.SMBus(1)
+        self.address = address
+        self.wm = 0x00
+        self.pds = 0x00
+        self.verbose = verbose
+        self.dynamic_range = dynamic_range
+
+    def deinit(self):
+        self.bus.close()   
+
+    def set_number(self, number):
+        if not isinstance(number, int):
+            print("На вход ЦАП можно подавать только целые числа")
+
+        if not (0 <= number <= 4095):
+            print("Число выходит за разраядность MCP4752 (12 бит)")
+
+        first_byte = self.wm | self.pds | number >> 8
+        second_byte = number & 0xFF
+        self.bus.write_byte_data(0x61, first_byte, second_byte)
+
+        if self.verbose:
+            print(f"Число: {number}, отправленные по I2C данные: [0x{(self.address << 1):02X}, 0x{first_byte:02X}, 0x{second_byte:02X}]\n")      
+
+    def set_voltage(self, voltage):
+        if not (0.0 <= voltage <= self.dynamic_range):
+            print("Выход за пределы динамического диапазона.")
+        number = ((voltage / self.dynamic_range) * 4095) 
+
+        self.set_number(number)   
+
+        if self.verbose:
+            print(f"Напряжение: {voltage}В -> Число для ЦАП: {number}")
+
+if __name__ == "__main__":
+    try:
+        while True:
+            try:
+                voltage = float(input("Введите напряжение в вольтах:"))
+
+                mcp = MCP4725(3.290, 0x61, True)
+
+            except ValueError:
+                print("Вы ввели не число. Попробуйте еще раз.")
+    finally:
+        mcp.deinit()
+                    
+                  
